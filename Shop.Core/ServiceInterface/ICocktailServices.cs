7459ea9fc94ffac616b9a2bd03cce693d974640a
NadezdaMarenkova <92782996+NadezdaMarenkova@users.noreply.github.com>
2024-05-13 12:23:57 +0300

    Add Cocktail

---
+++ Shop.Core/ServiceInterface/ICocktailServices.cs
@@ -0,0 +1,15 @@
+﻿using Shop.Core.Dto.CoctailsDtos;
+using Shop.Core.Dto.OpenWeatherDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Core.ServiceInterface
+{
+    public interface ICocktailServices
+    {
+        Task<CocktailResultDto> GetCocktails(CocktailResultDto dto);
+    }
+}
