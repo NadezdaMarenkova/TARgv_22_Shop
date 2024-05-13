68f46e4593451acc04800589a29620ccc21e146e
NadezdaMarenkova <92782996+NadezdaMarenkova@users.noreply.github.com>
2024-05-13 12:03:45 +0300

    Add OpenWeather

---
+++ Shop.ApplicationServicess/Services/WeatherForecastServices.cs
@@ -0,0 +1,34 @@
+﻿using Shop.Core.Dto.OpenWeatherDtos;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net;
+using System.Text.Encodings.Web;
+using Nancy.Json;
+using Shop.Core.ServiceInterface;
+
+namespace Shop.ApplicationServices.Services
+{
+    public class WeatherForecastServices : IWeatherForecastServices
+    {
+        public async Task<OpenWeatherResultDto> OpenWeatherResult(OpenWeatherResultDto dto)
+        {
+            string idOpenWeather = "45f47219199264e9a30f86bb1fd13939";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={dto.City}&units=metric&appid={idOpenWeather}";
+
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(url);
+                OpenWeatherResponseRootDto weatherResult = new JavaScriptSerializer().Deserialize<OpenWeatherResponseRootDto>(json);
+
+                dto.City = weatherResult.Name;
+                dto.Temp = weatherResult.Main.Temp;
+                dto.FeelsLike = weatherResult.Main.Feels_like;
+                dto.Humidity = weatherResult.Main.Humidity;
+                dto.Pressure = weatherResult.Main.Pressure;
+                dto.WindSpeed = weatherResult.Wind.Speed;
+                dto.Description = weatherResult.Weather[0].Description;
+            }
+
+            return dto;
+        }
+    }
+}
