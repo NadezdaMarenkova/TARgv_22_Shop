b63adf6a7d9cdf9c30beaf5c200261d700f79e86
NadezdaMarenkova <92782996+NadezdaMarenkova@users.noreply.github.com>
2024-05-13 11:47:06 +0300

    Add project files.

---
+++ Shop.ApplicationServices/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Shop.ApplicationServices.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
