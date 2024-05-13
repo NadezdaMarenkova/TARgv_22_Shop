51dacdc917b81a9415e49e860287de8d41ebf37b
NadezdaMarenkova <92782996+NadezdaMarenkova@users.noreply.github.com>
2024-05-13 12:26:56 +0300

    Add AccuWeather

---
+++ Shop.Core/ServiceInterface/IAccuWeatherServices.cs
@@ -0,0 +1,9 @@
+﻿using Shop.Core.Dto.AccuWeatherDtos;
+
+namespace Shop.Core.ServiceInterface
+{
+    public interface IAccuWeatherServices
+    {
+        public Task<AccuWeatherResultDto> AccuWeatherResult(AccuWeatherResultDto dto);
+    }
+}
